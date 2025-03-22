fb069fc2fc4f30c9f666acef916bc9379dcd24ab
M.Farar <mostafafarar2030@gmail.com>
2025-03-23 00:47:54 +0200

    add assets , font and images

---
+++ lib/views/responsivedash_view.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/widgets/adaptive_layout.dart';
+import 'package:responsive_dash_board/widgets/dashboard_desktoplayout.dart';
+
+class ResponsivedashView extends StatelessWidget {
+  const ResponsivedashView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: AdaptiveLayout(
+        mobilelayout: (context) => const SizedBox(),
+        tabletlayout: (context) => const SizedBox(),
+        desktoplayout: (context) => const DashboardDesktopLayout(),
+      ),
+    );
+  }
+}
